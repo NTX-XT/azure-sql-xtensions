ca8359c096b435e88712b20cc95b4a6c3b5df5ec
John Pouliezos <jpouliezos@gmail.com>
2022-05-10 19:40:29 +0300

    Fixed connection pooling issue

--- runScript/index.ts
+++ runScript/index.ts
@@ -3,27 +3,39 @@ import * as sql from 'mssql'
 import { ConnectionStringBuilder, ConnectionStringDetails } from "../shared/connectionStringBuilder";
 
 const runScript: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    // const templates : {[key: string]: string} = {
-    //     createUser: "CREATE LOGIN [{{USERNAME}}] WITH PASSWORD = N'{{PASSWORD}}'; ALTER LOGIN [{{USERNAME}}] ENABLE",
-    //     createLogin: "CREATE USER {{USERNAME}} FOR LOGIN {{USERNAME}} WITH DEFAULT_SCHEMA = dbo; EXEC sp_addrolemember N'db_owner', N'{{USERNAME}}'",
-    //     connString: "Server=tcp:nintexsolutions.database.windows.net,1433;Initial Catalog={{DBNAME}};Persist Security Info=False;User ID={{USERNAME}};Password={{PASSWORD}};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",        
-    // }
-
-    const connectionScript = ConnectionStringBuilder.build(req.body.connection as ConnectionStringDetails)
-    sql.connect(connectionScript)
-        .then((pool: sql.ConnectionPool) => {
-            pool.request().query(req.body.script).then((result: sql.IResult<any>) => {
-                context.res = {
-                    status: 200, /* Defaults to 200 */
-                };
-            })
-        })
-        .catch((err: Error) => {
-            context.res = {
-                status: 500, /* Defaults to 200 */
-                body: err.message
-            };
-        })
+    try {
+        const connDetails = req.body.connection as ConnectionStringDetails
+        const config: sql.config = {
+            user: connDetails.userName,
+            password: connDetails.password,
+            database: connDetails.dbName,
+            server: connDetails.server,
+            options: {
+                encrypt: true,
+                trustServerCertificate: false,
+            }
+        }
+        context.log.info(`connecting to: ${config.database}@${config.server} as ${config.user}`);
+        const pool = await sql.connect(config);
+        context.log.info(`executing script ${req.body.script}`);
+        const result = await pool.query(req.body.script);
+        pool.close();
+        context.log.info(result.output)
+        context.res = {
+            status: 200, /* Defaults to 200 */
+            body: result.output
+        };
+    }
+    catch (err: any) {
+        const error = (err.originalError.errors)
+            ? (err.originalError).errors.join(' // ')
+            : err.message
+        context.log.error(error)
+        context.res = {
+            status: 500, /* Defaults to 200 */
+            body: error
+        };
+    }
 };
 
 export default runScript;
